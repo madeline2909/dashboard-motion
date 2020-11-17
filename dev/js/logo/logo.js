353c710f1459be5b5ba059f3edd20e63298e768c
madeline2909 <63311619+madeline2909@users.noreply.github.com>
2020-11-17 21:35:51 +0700

    wings

--- dev/js/logo/logo.js
+++ dev/js/logo/logo.js
@@ -13,7 +13,9 @@ export function logoAnimation(){
     // .from(".rings",{duration:1, scale:0, stagger:-0.5, ease: "back.out(1.7)"},"-=2")
     .fromTo(".badge-lines",{drawSVG:"0% 0%"},{duration:1, drawSVG: "100%", stagger:-0.05}, "badge logo")
     .fromTo(".badge-lines-2",{drawSVG:"0% 0%"},{duration:1, drawSVG: "100%", stagger:0.05}, "badge logo")
-    .from("#badge",{scale: 5});
+    .from("#badge",{scale: 4.5}, "-=.5")
+    .from("#left-wing",{duration:0.5, x:200, alpha :0}, "wings")
+    .from("#right-wing",{duration:0.5, x:-200, alpha :0}, "wings");
 
     return logoTL;
 
