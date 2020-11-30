895f029eaf323806de8cf5582ff3c33c94823c2c
madeline2909 <63311619+madeline2909@users.noreply.github.com>
2020-11-30 09:46:43 +0700

    half way through the mode change anim

--- dev/js/dashboard/mode-change.js
+++ dev/js/dashboard/mode-change.js
@@ -12,28 +12,114 @@ const modeChangeTL = gsap.timeline();
 
 export function modeChange() {
     modeChangeTL
-    .add(rpmMorph(), "speed and rpm")
-    .add(speedMorph(), "speed and rpm")
+    .add(rpmAndMile(), "mode change")
+    .add(speedAndIcon(), "mode change")
+    .add(road(), "mode change")
     return modeChangeTL;
 }
 
 //tl for the rpm animations
-const rpmMorphTL = gsap.timeline();
+const rpmAndMileTL = gsap.timeline();
 
-function rpmMorph() {
-    rpmMorphTL
+function rpmAndMile() {
+    rpmAndMileTL
+        // .to("#rpm-outline", {
+        //     duration: 1,
+        //     morphSVG: 
+        //     {shape: "#rpm-circle-outline",
+        //     type:"rotational"}
+        // })
+        .from("#total-miles", {
+            duration: 0.75,
+            motionPath:
+            {
+                path:"#mile-motion-path",
+                align: "#mile-motion-path",
+                alignOrigin: [0.5, 0.5]
+            }
+        })
+        .to("#rpm-circle-outline", {
+            duration:0.01,
+            stroke:"#fff"
+        })
+        .to("#rpm-numbers", {
+            duration: 0.5,
+            alpha:0
+        },"-=.25")
         .to("#rpm-outline", {
             duration: 1,
-            morphSVG: "#rpm-circle-outline"
-        })
-}
-
-const speedMorphTL = gsap.timeline();
-
-function speedMorph() {
-    speedMorphTL
-        .from("#speed-circle-bg", {
+            drawSVG: "0%"
+        },"-=.5", "rpm and mile")
+        .from("#rpm-circle-outline", {
             duration: 1,
-            alpha:0
-        })
+            drawSVG: "0%"
+        },"-=.5", "rpm and mile")
+        
+        
+
+    // MotionPathHelper.create("#total-miles");
+    //return rpmAndMileTL;
+    
 }
+
+const roadTL = gsap.timeline();
+
+function road() {
+    roadTL
+        .to("#road", {
+            duration: 0.01,
+            stroke: "#fff"
+        })
+        .fromTo(".road", 
+        {drawSVG:"20% 0%"}, {drawSVG:"80% 100%", repeat:1, ease:"none",duration:1}, "-=.25")
+        .to("#road", {
+            stroke: "none",
+            duration:0.5,
+            fill:"#fff"
+        },"-=0.25")
+
+    return roadTL;
+}
+
+const speedAndIconTL = gsap.timeline();
+
+function speedAndIcon() {
+    speedAndIconTL
+        .to("#speed-circle-bg", {
+            duration: 0.5,
+            fill:"#fff"
+        })
+        .to("#speed-value", {
+            fill: "#2D0A44",
+            duration: 1,
+            scale: 0.7,
+            motionPath:
+            {
+                path:"#speed-motion-path",
+                align: "#speed-motion-path",
+                alignOrigin: [0.5, 0.5]
+            }
+        }, "speed")
+        .to("#KMH", {
+            fill: "#2D0A44",
+            duration: 1,
+            motionPath:
+            {
+                path:"#kmh-motion-path",
+                align: "#kmh-motion-path",
+                alignOrigin: [0.5, 0.5]
+            }
+        }, "speed")
+        .to("#warning-icons", {
+            motionPath:
+            {
+                path:"#icons-motion-path",
+                align: "#icons-motion-path",
+                alignOrigin: [0.5, 0.5]
+            }
+        }, "speed")
+
+    // MotionPathHelper.create("#warning-icons");
+    //return speedAndIconTL;
+}
+
